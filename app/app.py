78a80bf67ffef9c5fc01143e4701dd89ec9f6847
Alisher <alisherkaman7@gmail.com>
2026-05-17 14:19:51 +0500

    Add Python microservice and Dockerfile

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "SRE Capstone Project - V1 is running!"
+
+@app.route('/health')
+def health_check():
+    # Эту конечную точку потом будет дергать Kubernetes или автоскейлер
+    return {"status": "UP"}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
