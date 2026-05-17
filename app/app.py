ec494c546583157960f34e0102e56e46d2432180
Alisher <alisherkaman7@gmail.com>
2026-05-17 15:15:25 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "SRE Capstone Project - V1 is running!"
+    return "SRE Capstone Project - V2 is running!"
 
 @app.route('/health')
 def health_check():
